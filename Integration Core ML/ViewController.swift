7367c74886073e124401b4968887d8d308d13b7f
ekaras <key.cpp@gmail.com>
2023-01-10 20:38:23 +0100

    0

---
+++ Integration Core ML/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Integration Core ML
+//
+//  Created by Евгений Карась on 10.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
